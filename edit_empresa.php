36f3c4f28f9cc6e8b7ef4e09a584fea984eb754f
davi-ramos72 <72760642+davi-ramos7@users.noreply.github.com>
2021-06-08 23:59:05 -0300

    Update edit_empresa.php

--- edit_empresa.php
+++ edit_empresa.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 include_once("conexao.php");
-$id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
+$id = filter_input(INPUT_GET, 'p', FILTER_SANITIZE_NUMBER_INT);
 $result_usuario = "SELECT * FROM lista_de_empresas WHERE id = '$id'";
 $resultado_usuario = mysqli_query($con, $result_usuario);
 $row_usuario = mysqli_fetch_assoc($resultado_usuario);
@@ -13,25 +13,34 @@ $row_usuario = mysqli_fetch_assoc($resultado_usuario);
 		<title>CRUD - Editar</title>		
 	</head>
 	<body>
-		<a href="cad_usuario.php">Cadastrar</a><br>
-		<a href="index.php">Listar</a><br>
-		<h1>Editar Usuário</h1>
 		<?php
 		if(isset($_SESSION['msg'])){
 			echo $_SESSION['msg'];
 			unset($_SESSION['msg']);
 		}
 		?>
-		<form method="POST" action="proc_edit_usuario.php">
-			<input type="hidden" name="id" value="<?php echo $row_usuario['id']; ?>">
-			
-			<label>Nome: </label>
-			<input type="text" name="nome" placeholder="Digite o nome completo" value="<?php echo $row_usuario['nome']; ?>"><br><br>
-			
-			<label>E-mail: </label>
-			<input type="email" name="email" placeholder="Digite o seu melhor e-mail" value="<?php echo $row_usuario['email']; ?>"><br><br>
-			
-			<input type="submit" value="Editar">
+		<!-- <form method="POST" action="proc_edit_usuario.php"> -->
+        <form class="formulario_empresa" id="edit_empresa_form" method="post">
+            <table id="table_empresa">
+                <tr>
+                    <td><input type="hidden" name="id" value="<?php echo $row_usuario['id']; ?>"></td><td></td>
+                </tr>
+                <tr>
+                    <td>Nome: </td><td><input type="text" name="edit_nome" id="nome_empresa" placeholder="Digite o nome da empresa/pessoa física" value="<?php echo $row_usuario['nome']; ?>"></td>
+                </tr>
+                <tr>
+                    <td>Atividade: </td><td><input type="text" name="edit_ativ" id="atividade_empresa" placeholder="Digite a atividade" value="<?php echo $row_usuario['atividade']; ?>"></td>
+                </tr>
+                <tr>
+                    <td>Cnpj/Cpf: </td><td><input type="text" name="edit_cnpj" placeholder="Digite o CNPJ ou CPF" value="<?php echo $row_usuario['cnpj_cpf']; ?>"></td>
+                </tr>
+                <tr>
+                    <td>Endereço: </td><td><input type="text" name="edit_end" id="end_empresa" placeholder="Digite o endereço da empresa/pessoa física" value="<?php echo $row_usuario['endereco']; ?>"></td>
+                </tr>
+                <tr>
+                    <td><input type="submit" value="Editar"></td><td></td>
+                </tr>
+            </table>			
 		</form>
 	</body>
 </html>
